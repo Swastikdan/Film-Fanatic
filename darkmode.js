6aed01d02567882a7529ca27d4d86e5eaa8faa7e
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2023-04-15 03:03:29 +0530

    build problem changed

--- darkmode.js
+++ darkmode.js
@@ -1,6 +1,18 @@
+// script 1
+  // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+  if (
+    localStorage.getItem("color-theme") === "dark" ||
+    (!("color-theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+
 
   // script 2
-      var themeToggleDarkIcon = document.getElementById(
+  var themeToggleDarkIcon = document.getElementById(
         "theme-toggle-dark-icon"
       );
       var themeToggleLightIcon = document.getElementById(
--- search-main.js
+++ search-main.js
@@ -1,4 +1,4 @@
 import './src/js/search.js'
 import './src/js/back-to-top.js'
-import './src/js/darkmode.js'
+// import './src/js/darkmode.js'
 import './src/js/footer.js'
