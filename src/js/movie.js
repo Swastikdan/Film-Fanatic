275809fd6d1d67167b3742b1ef59a0b3a931108d
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2023-04-17 09:36:13 +0530

    movie commit #2

--- src/js/movie.js
+++ src/js/movie.js
@@ -39,12 +39,14 @@ for (let i = 0; i < videos.length; i++) {
 const trailersHTML= videoKeys.map(videoKey => {
     // const youtubeUrl = `https://www.youtube.com/embed/${videoKey}`;
     return `
-    <div class="relative cursor-pointer inline-block w-3/5 h-96" data-video-id="${videoKey}" onclick="loadVideo(this)">
-  <img class="w-full h-full" src="https://img.youtube.com/vi/${videoKey}/maxresdefault.jpg" alt="Thumbnail">
-  <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-red-500">
-  <svg class="w-20 h-20 " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 68 48"><path d="M66.52 7.74c-.78-2.93-2.49-5.41-5.42-6.19C55.79.13 34 0 34 0S12.21.13 6.9 1.55c-2.93.78-4.63 3.26-5.42 6.19C.06 13.05 0 24 0 24s.06 10.95 1.48 16.26c.78 2.93 2.49 5.41 5.42 6.19C12.21 47.87 34 48 34 48s21.79-.13 27.1-1.55c2.93-.78 4.64-3.26 5.42-6.19C67.94 34.95 68 24 68 24s-.06-10.95-1.48-16.26z" fill="red"/><path d="M45 24 27 14v20" fill="white"/></svg>
+    <li class=" rounded-xl">
+    <div class="relative cursor-pointer inline-block rounded-xl  " data-video-id="${videoKey}" onclick="loadVideo(this)">
+    <img class="w-full h-full rounded-xl" src="https://img.youtube.com/vi/${videoKey}/maxresdefault.jpg" alt="Thumbnail">
+    <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-red-500">
+    <svg class="w-20 h-20 " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 68 48"><path d="M66.52 7.74c-.78-2.93-2.49-5.41-5.42-6.19C55.79.13 34 0 34 0S12.21.13 6.9 1.55c-2.93.78-4.63 3.26-5.42 6.19C.06 13.05 0 24 0 24s.06 10.95 1.48 16.26c.78 2.93 2.49 5.41 5.42 6.19C12.21 47.87 34 48 34 48s21.79-.13 27.1-1.55c2.93-.78 4.64-3.26 5.42-6.19C67.94 34.95 68 24 68 24s-.06-10.95-1.48-16.26z" fill="red"/><path d="M45 24 27 14v20" fill="white"/></svg>
+    </div>
   </div>
-</div>
+  </li>
 
   
     `;
@@ -59,10 +61,7 @@ fetch(`https://www.omdbapi.com/?i=${movie.imdb_id}&apikey=22b88799`)
   .then(response => response.json())
   .then(data => {
     
-    const movieTrailerHTML = `<ul class="slider-container">
-    ${trailersHTML}
-    </ul>`
-    movieTrailerContainer.innerHTML = movieTrailerHTML;
+    movieTrailerContainer.innerHTML = trailersHTML;
     const movieDetailsHTML = `
       <div class="movie-poster my-4 ">
         <img class="w-60" src="${CDN}${IMAGE_URL}${movie.poster_path}" alt="${movie.title}">
