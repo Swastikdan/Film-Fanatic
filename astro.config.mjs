2aec59e09ef7f970a1c8327c27cf8e8b3918dce5
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2024-02-04 00:26:57 +0530

    node removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import node from "@astrojs/node";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap(), react()],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  
 });
