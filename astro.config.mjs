408a1208e6441c82234bf940451f2b4cbc22cd8d
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2024-02-04 10:39:45 +0530

    vercel intergation fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/serverless';
-import vercel from '@astrojs/vercel/static';
+// import vercel from '@astrojs/vercel/static';
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "static",
+  output: "server",
   adapter: vercel(),
 });
