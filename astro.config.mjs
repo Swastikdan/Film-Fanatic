28cb3100be1e38ca55b4189d07a53a95891cc214
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2024-02-04 10:37:44 +0530

    vercel added

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server",
+  output: "static",
+  adapter: vercel(),
 });
