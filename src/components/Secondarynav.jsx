66bc3156ce7109ceae2b566a0babbcc09b00b7f6
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2024-05-12 21:03:57 +0530

    tv page added partially 🐱‍🚀

--- src/components/Secondarynav.jsx
+++ src/components/Secondarynav.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Share from './Share';
 import Goback from './Goback';
-export default function Secondarynav({ movie, url, id, slug ,color }) {
+export default function Secondarynav({ data, url, id, slug ,color , type }) {
   useEffect(() => {
     const div = document.querySelector('#dynmaic-bg');
     div.style.backgroundColor = color;
@@ -10,9 +10,9 @@ export default function Secondarynav({ movie, url, id, slug ,color }) {
   const [currentPath, setCurrentPath] = useState(null);
 
   useEffect(() => {
-    if(window.location.pathname == `/movie/${id}/${slug}/media`){
+    if(window.location.pathname == `/${type}/${id}/${slug}/media`){
       setCurrentPath('media')
-    }else if(window.location.pathname == `/movie/${id}/${slug}/cast`){
+    }else if(window.location.pathname == `/${type}/${id}/${slug}/cast`){
       setCurrentPath('cast')
     }
   }, [window.location.pathname]);
@@ -23,26 +23,26 @@ export default function Secondarynav({ movie, url, id, slug ,color }) {
       <div class='mx-auto block max-w-screen-2xl items-center px-2 md:px-10 xl:px-20'>
         <div class='flex items-center space-x-0'>
           <img
-            src={`https://media.dev.to/cdn-cgi/image/width=400,height=600,fit=cover,gravity=auto,format=auto/${movie.poster_path ? `https://image.tmdb.org/t/p/original/${movie.poster_path}` : `https://via.placeholder.com/500x750?text=${movie.title}`}`}
-            alt={movie.title}
+            src={`https://media.dev.to/cdn-cgi/image/width=400,height=600,fit=cover,gravity=auto,format=auto/${data.poster_path ? `https://image.tmdb.org/t/p/original/${data.poster_path}` : `https://via.placeholder.com/500x750?text=${data.title}`}`}
+            alt={data.title}
             class='hidden h-28 w-20 rounded-lg object-cover ring-2 ring-gray-200 md:block'
           />
 
           <div class='flex w-full flex-col px-2 sm:px-3 md:px-5'>
             <div class='flex w-full items-center justify-between pb-2 '>
               <h1 class=' text-start text-lg  font-bold md:text-3xl'>
-                {movie.title} ({movie.year})
+                {data.title} ({data.year})
               </h1>
-              {/* <Share title={movie.title} url={url} /> */}
+              {/* <Share title={data.title} url={url} /> */}
             </div>
             <div class='flex w-full items-center justify-between pb-2 '>
               <Goback
                 text='Back to main'
                 type={'icon'}
-                link={`/movie/${id}/${slug}`}
+                link={`/${type}/${id}/${slug}`}
                 bg={false}
               />
-              <Share title={movie.title} url={url} />
+              <Share title={data.title} url={url} />
             </div>
           </div>
         </div>
@@ -50,12 +50,12 @@ export default function Secondarynav({ movie, url, id, slug ,color }) {
 
       <div className='mt-3 flex w-full items-center justify-center gap-2 bg-gray-800 py-3  text-base font-semibold text-white'>
         <a
-          href={`/movie/${id}/${slug}/media`}
+          href={`/${type}/${id}/${slug}/media`}
           className={` rounded-lg px-4 py-1  hover:ring-1 hover:ring-gray-200 ${currentPath == 'media' ? 'ring-1 ring-gray-200' : ''} `}>
           Media{' '}
         </a>
         <a
-          href={`/movie/${id}/${slug}/cast`}
+          href={`/${type}/${id}/${slug}/cast`}
           className={` rounded-lg px-4 py-1  hover:ring-1 hover:ring-gray-200 ${currentPath == 'cast' ? 'ring-1 ring-gray-200' : ''} `}>
           Cast & Crew{' '}
         </a>
