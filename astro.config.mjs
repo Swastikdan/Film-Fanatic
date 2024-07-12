e6ace1239cd76e4de25ef378e6cb948a573f2f98
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2024-07-12 08:56:25 +0530

    error fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
+
+// import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
   adapter: vercel(),
+  // output: "server",
+  // adapter: node({
+  //   mode: "standalone"
+  // })
 });
