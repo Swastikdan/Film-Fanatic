b37cc5a18c5c311a8af0b12976cc12d72d89393b
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2024-10-06 00:52:04 +0530

    vercel build fix

--- src/pages/_robots.txt.ts
+++ src/pages/_robots.txt.ts
@@ -31,7 +31,6 @@ Disallow: /
 Sitemap: ${new URL('sitemap-index.xml', import.meta.env.SITE).href}`.trim();
 
 export const GET: APIRoute = () => {
-  console.log(robotsTxt);
   return new Response(robotsTxt, {
     headers: {
       'Content-Type': 'text/plain; charset=utf-8',
