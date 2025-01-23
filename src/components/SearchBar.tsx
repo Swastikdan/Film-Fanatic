ba8c6b4d7754b67616c209661c2627fd46d77b9d
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2025-01-23 12:37:19 +0530

    autofocus removed

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -57,7 +57,7 @@ export function Searchbar({
           placeholder="👀 What movie, show, person? Let's find it!"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
-          autoFocus
+          
           className="peer block h-12 w-full bg-background pr-8 ps-10 placeholder:text-sm enabled:bg-background sm:h-11 placeholder:md:text-base"
           aria-label="Search Input"
         />
