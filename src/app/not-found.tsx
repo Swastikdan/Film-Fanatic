15b2c145eca92937cb4c046e805ef50fda8fcb76
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2025-06-30 17:44:32 +0530

    loading and not found page fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 
 export default function NotFoundPage() {
   return (
-    <div className="grid h-full min-h-[calc(100vh-215px)] place-content-center items-center justify-center md:min-h-[calc(100vh-175px)]">
+    <div className="grid h-full min-h-screen place-content-center items-center justify-center">
       <h1 className="font-heading text-xl tracking-widest uppercase">
         404 | Not Found
       </h1>
--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -1,6 +1,6 @@
 export function Spinner() {
   return (
-    <div className="grid h-full min-h-[calc(100vh-215px)] place-content-center items-center justify-center md:min-h-[calc(100vh-175px)]">
+    <div className="grid h-full min-h-screen place-content-center items-center justify-center">
       <svg viewBox="25 25 50 50" className="loader">
         <circle r="20" cy="50" cx="50"></circle>
       </svg>
