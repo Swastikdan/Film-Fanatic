ecc8164dad41c9f5a7739d5f679ed943e30672b6
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2025-07-08 08:34:59 +0530

    eslint fix

--- src/components/ui/image.tsx
+++ src/components/ui/image.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useCallback, memo, useRef, useEffect } from "react";
+import { useState, useCallback, memo, useEffect } from "react";
 import { cn } from "@/lib/utils";
 import Image, { type ImageProps } from "next/image";
 
